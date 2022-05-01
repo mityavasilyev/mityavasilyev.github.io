5e5e2ee347da56adb609ede9a625962130b5aa71
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-05-01 14:56:29 +0300

    Added libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Box, Button} from "@mui/material";
+import Mojsbouncer from "./components/animations/Mojsbouncer";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p>I promise. There will be something here soon</p>
+        <Button>Clickity click</Button>
+        <Box sx={{height: "200px"}}>
+
+        </Box>
+        <Mojsbouncer duration={2000}/>
       </header>
     </div>
   );
